af412e82b9685c78727505c114b1ff9e67b14b4d
Masaaki Wada <wadam@uniontool.co.jp>
2019-09-23 11:35:25 +0900

    remove: wear プロジェクト

--- Sample/SampleProjectAndroidWear/common/src/main/java/uniontool/co/jp/whs_2_android_with_wear/Constants.java
+++
@@ -1,9 +0,0 @@
-package uniontool.co.jp.whs_2_android_with_wear;
-
-public class Constants {
-    public static final String PATH_NOTIFICATION = "/whs2android";
-    public static final String PATH_DISMISS = "/whs2dismiss";
-    public static final String KEY_HEARTRATE = "heartrate";
-    public static final String KEY_MOVING = "moving";
-    public static final String KEY_TEMPERATURE = "temperature";
-}
